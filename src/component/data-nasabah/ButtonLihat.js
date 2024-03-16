10ef33ec61c9365a680a05f80c108f8febed93ca
kedarnacha <kedaranugerahnachaputra@gmail.com>
2024-03-16 23:07:33 +0700

    tuh fix

--- src/component/data-nasabah/ButtonLihat.js
+++ src/component/data-nasabah/ButtonLihat.js
@@ -1,137 +1,48 @@
 import React, { useEffect, useState, useRef } from "react";
-import $ from "jquery";
-import "datatables.net-dt/js/dataTables.dataTables";
-import "datatables.net-dt/css/jquery.dataTables.min.css";
-
 import axios from "axios";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../config/firebase";
-import { useForm } from "react-hook-form";
 
-export default function ButtonLihat(user_id) {
-  const [authUser, setAuthUser] = useState(null);
-  const [token, setToken] = useState([]);
-  const [dataNasabah, setDataNasabah] = useState([]);
+export default function ButtonLihat(item) {
+  console.log(item)
+  const [token, setToken] = useState();
   const [formData, setFormData] = useState({});
-  const [selectedImage, setSelectedImage] = useState(null);
   const modalRef = useRef(null);
-
-  const form = useForm({
-    defaultValues: {
-      user_id: formData.user_id,
-      nama: formData.nama,
-      nomor_handphone: formData.nomor_handphone,
-      avatar: formData.avatar,
-      alamat: formData.alamat,
-    },
-  });
-
-  const addForm = useForm({
-    nama: "",
-    nomor_handphone: 0,
-    thumbnail: "",
-    alamat: "",
-    stok: 0,
-  });
-
-  const getDataNasabah = async () => {
-    const headers = { Authorization: `Bearer ${token}` };
-    try {
-      const response = await axios.get("https://devel4-filkom.ub.ac.id/bank-sampah/user?status=1&isPagination=false", { headers });
-      setDataNasabah(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const getPermintaanID = async (ids) => {
-    console.log(ids.id); //cek isi ids
-    console.log(ids.nama); //cek isi ids
-
-    const headers = { Authorization: `Bearer ${token}` };
-    try {
-      const response = await axios.get(
-        `https://devel4-filkom.ub.ac.id/bank-sampah/user/${ids}/history`,
-        {
-          user_id: ids,
-          nama: formData.nama,
-          nomor_handphone: formData.nomor_handphone,
-          // tgl_verifikasi: "?",
-          alamat: formData.alamat,
-          avatar: formData.avatar,
-        },
-        { headers }
-      );
-      setFormData(response.data.data);
-      console.log(response.data.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
 
   const handleDetailClick = async (id) => {
     try {
-      await getPermintaanID(id);
+      const headers = { Authorization: `Bearer ${token}` };
+      const response = await axios.get(
+        `https://devel4-filkom.ub.ac.id/bank-sampah/user/${id}/history`,
+        { headers }
+      );
+      setFormData(response.data.user);
+      console.log(response.data.user);
       modalRef.current.open = true;
     } catch (error) {
       console.error("Error handling detail click:", error);
     }
   };
 
-  const evidenceRef = useRef(null);
-  const handleGambar = (e) => {
-    const gambarmu = e.target.files?.[0];
-    if (gambarmu) {
-      const imageUrl = URL.createObjectURL(gambarmu);
-      setSelectedImage(imageUrl);
-    }
-    form.setValue("thumbnail", gambarmu);
-  };
-
-  const handleUpdate = async (formData) => {
-    const headers = { Authorization: `Bearer ${token}`, "Content-Type": "multipart/form-data" };
-    try {
-      const formDataWithFile = new FormData();
-
-      Object.entries(formData).forEach(([key, value]) => {
-        if (key !== "avatar" && key !== "user_id") {
-          formDataWithFile.append(key, value);
-        }
-      });
-      if (formData.thumbnail) {
-        formDataWithFile.append("avatar", formData.avatar);
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setToken(user.accessToken);
       }
-      for (var pair of formDataWithFile.entries()) {
-        console.log(pair[0] + ", " + pair[1]);
-      }
-      const response = await axios.put(`https://devel4-filkom.ub.ac.id/bank-sampah/user/${formData.user_id}`, formDataWithFile, { headers });
-      if (response.status === 200) {
-        alert("Berhasil mengubah isi ");
-      } else {
-        alert("Gagal mengubah isi ");
-      }
-      console.log(response);
-      form.reset();
-      window.location.reload();
-    } catch (error) {
-      console.error("Error program:", error);
-    }
-  };
+    });
+    return () => {
+      listen();
+    };
+  }, [formData]);
 
-  // Function to format the date
-  const formatDate = (dateObj) => {
-    const { day, month, year } = dateObj;
-    return `${day}/${month}/${year}`;
-  };
   return (
     <>
-      <button className="btn btn-primary btn-sm mt-1 mx-2" data-toggle="modal" data-target="#modal_liat_data_nasabah" onClick={() => handleDetailClick(user_id)}>
+      <button className="btn btn-primary btn-sm mt-1 mx-2" data-toggle="modal" data-target={`#modal_liat_data_nasabah_${item.id}`} onClick={() => handleDetailClick(item.id)}>
         Lihat
       </button>
 
       {/* MODAL LIHAT */}
-      <div className="modal fade" ref={modalRef} id="modal_liat_data_nasabah" data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div className="modal fade" ref={modalRef} id={`modal_liat_data_nasabah_${item.id}`} data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby={`staticBackdropLabel_${item.id}`} aria-hidden="true">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header ">
@@ -144,43 +55,48 @@ export default function ButtonLihat(user_id) {
               </button>
             </div>
             <div className="modal-body justify-content-center">
-              <div className="modal-image d-flex justify-content-center">
-                <img src={formData.avatar} width="50" height="50" alt="Avatar" />
-              </div>
-              <form className="m-5">
+              {/* Conditionally render the content when formData is populated */}
+              {formData && Object.keys(formData).length > 0 && (
                 <>
-                  <div className="form-group row">
-                    <label className="col-sm-5 col-form-label">ID Nasabah</label>
-                    <div className="col-sm-7">
-                      <div type="text" className="mt-2  font-weight-bold">
-                        : {formData.user_id}
+                  <div className="modal-image d-flex justify-content-center">
+                    {console.log(formData)}
+                    <img src={formData.avatar} width="50" height="50" alt="Avatar" />
+                  </div>
+                  <form className="m-5">
+                    <div className="form-group row">
+                      <label className="col-sm-5 col-form-label">ID Nasabah</label>
+                      <div className="col-sm-7">
+                        <div type="text" className="mt-2  font-weight-bold">
+                          {console.log(formData.user_id)}
+                          : {formData.user_id}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="form-group row">
-                    <label className="col-sm-5">Nama</label>
-                    <div className="col-sm-7">
-                      <div className=" font-weight-bold">: {formData.nama}</div>
+                    <div className="form-group row">
+                      <label className="col-sm-5">Nama</label>
+                      <div className="col-sm-7">
+                        <div className=" font-weight-bold">{console.log(formData.nama)}: {formData.nama}</div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="form-group row">
-                    <label htmlFor="inputPassword" className="col-sm-5 col-form-label">
-                      No HP / WA
-                    </label>
-                    <div className="col-sm-7">
-                      <div className="mt-2 font-weight-bold">: {formData.nomor_handphone}</div>
+                    <div className="form-group row">
+                      <label htmlFor="inputPassword" className="col-sm-5 col-form-label">
+                        No HP / WA
+                      </label>
+                      <div className="col-sm-7">
+                        <div className="mt-2 font-weight-bold">{console.log(formData.nomor_handphone)}: {formData.nomor_handphone}</div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="form-group row">
-                    <label htmlFor="inputPassword" className="col-sm-5 col-form-label">
-                      Alamat Nasabah
-                    </label>
-                    <div className="col-sm-7">
-                      <div className="font-weight-bold mt-2">: {formData.alamat}</div>
+                    <div className="form-group row">
+                      <label htmlFor="inputPassword" className="col-sm-5 col-form-label">
+                        Alamat Nasabah
+                      </label>
+                      <div className="col-sm-7">
+                        <div className="font-weight-bold mt-2">{console.log(formData.alamat)}: {formData.alamat}</div>
+                      </div>
                     </div>
-                  </div>
+                  </form>
                 </>
-              </form>
+              )}
             </div>
           </div>
         </div>
